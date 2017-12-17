71ff0e117b643dcfcf1b72e9be6c1f3c40f621a9
Ed Wright <ew_wrightiii@live.com>
2017-12-17 05:32:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   var emptyArray = []
   for(let i = 0; i < arrayOfMusicians.length; i++) {
-    
+    //Did not need nested loops
     emptyArray.push(arrayOfMusicians[i] + " plays " + arrayOfInstruments[i])
     }
   
   return emptyArray
+}
+
+function johnLennonFacts(arrayOfFacts) {
+  while()
 }
