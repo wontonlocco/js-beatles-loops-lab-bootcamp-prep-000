44d073328fa37b1c37588d0d518832debb156782
Ed Wright <ew_wrightiii@live.com>
2017-12-17 06:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ function theBeatlesPlay(arrayOfMusicians, arrayOfInstruments) {
   return emptyArray
 }
 
-function johnLennonFacts() {
-  var facts = [
+ var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",
   "He hated the sound of his own voice"
 ];
+function johnLennonFacts(facts) {
+ 
   var arrayOfStrings = []
   var y = 0;
   while(y < facts.length) {
